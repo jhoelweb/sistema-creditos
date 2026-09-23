754ad7fc9928bdd5996bf58e39f48397f5b1e1eb
unknown <diazjhoel91@gmail.com>
2026-09-22 19:11:58 -0600

    Evitar cache de paginas autenticadas

---
+++ app/Http/Middleware/NoCache.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class NoCache
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        return $response->withHeaders([
+            'Cache-Control' => 'no-cache, no-store, must-revalidate, max-age=0',
+            'Pragma' => 'no-cache',
+            'Expires' => '0',
+        ]);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->trustProxies(at: '*');
 
+        $middleware->append(\App\Http\Middleware\NoCache::class);
+
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
